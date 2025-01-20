99745f9bbcc6161ef0fb84f2cea25dbf93cdcd67
Siddharamayya M <66553684+mtptisid@users.noreply.github.com>
2025-01-20 20:54:54 +0530

    Create delete_entry_with_LV_name.sh

---
+++ FSTAB/delete_entry_with_LV_name.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###########################################################################
+######## Script for removing entry in FSTAB with LV Name                 ##
+######## Created By    : MATHAPATI Siddharamayya                         ##
+######## Creation Date : 20th January 2025                               ##
+######## Email         : msidrm455@gmail.com                             ##
+######## Version       : 2.0                                             ##
+###########################################################################
+
+#set -x
